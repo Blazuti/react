f9f34f0d20b2bdeaff71dfaf2ce61aafb80cbc8b
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-14 10:39:53 -0300

    estudo

--- app01/src/App.jsx
+++ app01/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import foto01 from './componentes/imgs/background001.jpg'
+import foto02 from './componentes/imgs/background002.jpg'
+
 
 export default function app(){
+  const imagem=()=>{
+    return <img src={foto01} alt="" />
+  }
+
   return(
     <>
-      <h1>Testando React</h1>
+     {imagem()}
+     <img src={foto02} alt="" />
     </>
   )
 }
