e7bfced72dcdff65d9e7e8c214b5205ce22d54f0
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-15 13:17:19 -0300

    teste

--- app01/src/componentes/estrutura/corpo.jsx
+++ app01/src/componentes/estrutura/corpo.jsx
@@ -3,10 +3,14 @@ import imagem from "../imgs/background002.jpg"
 
 export default function corpo(){
     return(
+        
         <>
-        <img src={imagem} alt="imagem caderno" />
+        <div className="imagem">
+        <img src={imagem} alt="imagem caderno"/> 
+        </div>
         </>
-    )       
+        
+    )         
         
     
 }
