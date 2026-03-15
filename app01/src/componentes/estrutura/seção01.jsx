8b9e75d4908f046c227622dc13750b9a2ad877c8
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-15 14:04:16 -0300

    Update seção01.jsx

--- app01/src/componentes/estrutura/seção01.jsx
+++ app01/src/componentes/estrutura/seção01.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function seção01(props){
+    
+    let [num, setNum] = useState(0)   
+    const decrementar = ()=>{
+        setNum(num + 1);
+    }
+   
     return(
         <>
         <p>youtube:{props.youtube}</p>
         <p>curso:{props.curso()}</p>
+        <p>{num}</p>
+        
+        <button onclick={decrementar}> decrementa</button>
         </>
     )
 }
